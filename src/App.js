28d777d936753863b98655395abf178dae0ed153
Jovan Jiv Cahiles <jovan@simpleclick.com.au>
2022-03-24 13:52:59 +0800

    added metamask authentication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const connect = async (onConnected) => {
+  if (!window.ethereum) {
+    alert("MetaMask not installed")
+    return
+  }
+
+  const accounts = await window.ethereum.request({
+    method: "eth_requestAccounts",
+  })
+
+  onConnected(accounts[0])
 }
 
-export default App;
+const isWalletConnected = async (onConnected) => {
+  if (window.ethereum) {
+    const accounts = await window.ethereum.request({
+      method: "eth_accounts",
+    })
+
+    if (accounts.length > 0) {
+      const account = accounts[0]
+      onConnected(account)
+      return
+    }
+  }
+}
+
+const Login = ({ setAddress }) => {
+  return (
+    <button onClick={() => connect(setAddress)} style={buttonStyle}>
+      Connect to MetaMask
+    </button>
+  )
+}
+
+const buttonStyle = {
+  padding: 20,
+  backgroundColor: "#ff7b00",
+  borderRadius: 10,
+  borderWidth: 0,
+  color: "#fff",
+  fontSize: 50,
+}
+
+function App() {
+  const [address, setAddress] = useState("")
+
+  useEffect(() => {
+    isWalletConnected(setAddress)
+  }, [])
+
+  return (
+    <div style={containerStyle}>
+      {address ? (
+        <div style={connectedStyle}>Connected with {address}</div>
+      ) : (
+        <Login setAddress={setAddress} />
+      )}
+    </div>
+  )
+}
+
+const connectedStyle = {
+  fontSize: 50,
+}
+
+const containerStyle = {
+  display: "flex",
+  height: "100vh",
+  alignItems: "center",
+  justifyContent: "center",
+}
+
+export default App
